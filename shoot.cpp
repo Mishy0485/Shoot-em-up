ca8929dece4d56b55559623f03a08c6045549fc1
Julia <ayite.julia06@gmail.com>
2024-11-25 11:01:25 +0100

    Add files via upload

---
+++ shoot.cpp
@@ -0,0 +1,15 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
+#include<iostream>
+using namespace std;
+
+// shoot em up : PROJET
+
+// création du shooter + destructeur ( class? )
+
+// déplacement
+
+// création des ennemis + destructeur : descendent ( si ligne soous laquelle mort auto ou deux modes?) ou immobile
+
+// collisions ( verif de boum )
+
